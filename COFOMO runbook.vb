52f61045cf61a9a899e2ade09df413827cc1c586
dioufmouhamed <diouf_mouhamed@outlook.com>
2024-09-24 14:07:43 +0200

    commit

---
+++ COFOMO runbook.vb
@@ -0,0 +1,52 @@
+COFOMO runbook 
+sprint1
+
+sprint2
+sprint3
+sprint4
+sprint5
+sprint6
+cmdb_ci_handheld_computing
+cmdb_ci_net_app_server
+cmdb_ci_ip_firewall
+cmdb_ci_san
+cmdb_ci_handheld_computing
+cmdb_ci_comm
+
+
+les stories du sprint6:
+    STRY0010365  : telephone intelligente (cmdb_ci_handheld_computing)
+    STRY0010373  : la telephonie v2 (cmdb_ci_comm)  if "manufacturer" is (Logitech or  Adesso  or Sennheiser) than Servicenow Sys_class is "alm_consumable"
+    STRY0010476  : Appliance (cmdb_ci_net_app_server)
+    STRY0010480  : Baston (cmdb_ci_ip_firewall)
+    STRY0010757  : SAN(cmdb_ci_san)
+    STRY0011076  : KVM ("cmdb_ci_peripheral" and type = "KVM switch")
+    STRY0011086  : les Tablette (cmdb_ci_handheld_computing)
+    STRY0011103
+
+
+
+    STRY0010365 
+    STRY0010373  
+    STRY0010476  
+    STRY0010480  
+    STRY0010757  
+    STRY0011076  
+    STRY0011086  
+
+les champs CI :
+name
+asset tag
+model
+mmanufacture
+life cycle status 
+life cycle stage 
+serial number
+hostname 
+ip address
+mmac address
+assigned to 
+managed by
+description
+loation picci
+environement 
--- check_in_instance.js
+++ check_in_instance.js
@@ -1,3 +1,56 @@
-
-
-
+(function(batch, output) {
+    for (var i = 0; i < batch.length; i++) {
+       var inputFonction = batch[i].u_fonction.trim(); // Value of the input columns concatenated with '|'.
+       var inputCadre = batch[i].u_cadre_utilisateur.trim();
+       var result = "";
+       // Access input column values by their names, ex: batch[i].column_name
+       
+       var data_function = {
+          "Accueil": "shared|||",
+          "Contingence": "shared|||",
+          "Formation": "shared|||",
+          "Ilôt": "shared|||",
+          "Individuel": "primary|||",
+          "Période de pointe": "shared|||peak period",
+          "Secondaire": "secondary|||",
+          "Laboratoire": "laboratoire|||",
+       };
+       var data_cadre_utilisateur = {
+        "Banctec":"|||banctec",
+        "BNA": "bna|||",
+        "dépannage":"shared|||depannage",
+        "Enquêtes":"laboratoire|||",
+        "Ergonomie":"primary|||medical",
+        "Poste Fantôme": "hors reseau rq",
+        "Fantôme": "hors reseau rq",
+        "Fantome": "hors reseau rq",
+        "ilot analyste VAO":"shared|||",
+        "Impression massive":"|||ininterruptible",
+        "Numériseur":"|||ininterruptible",
+        "Poste courtoisie":"bna|||",
+        "Poste de nuit":"|||ininterruptible",
+        "Reprodocmic":"|||ininterruptible",
+        "Résidence":"|||residence",
+        "SCCM":"laboratoire|||",
+        "WAAS":"laboratoire|||",
+        "Super Calculateur":"|||ininterruptible",
+        "Tenant de secours":"secondary|||tenant secours"    
+     };
+       if(inputFonction == "" && inputCadre == ""){
+          output[i] = "|||";
+       }
+       else if(inputFonction != ""){for (var key in data_function) {
+          if (key.includes(inputFonction)) {
+             output[i] = data_function[key];
+             break;
+          }
+       }}
+       else {
+          for (var keyCad in data_cadre_utilisateur) {
+             if (keyCad.includes(inputCadre)) {
+                output[i] = data_cadre_utilisateur[keyCad];
+                break;
+             } }
+          }
+       }
+    })(batch, output);
