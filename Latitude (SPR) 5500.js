6edd82e59e526e621a1f93cf18ccfd8819b58217
dioufmouhamed <diouf_mouhamed@outlook.com>
2025-01-12 18:23:45 +0000

    January

---
+++ Latitude (SPR) 5500.js
@@ -0,0 +1,15 @@
+Latitude (SPR) 5500
+
+// var model = new RQModelDell();
+// model.MigrateModeleRQ("model_id=717e8ffb97b00a50bf87b87e6253afc7");
+// gs.print(model.TABModele);
+
+
+Creer un document pour decrire la procedure de suppression des données sur la table 
+creer un document pour la verification des echeanciers
+procedurte pour verification etl et rouler les etls 
+
+les procedure pretes
+STRY0011245 pret pour test
+DFCT0010383 commentaire ajouté 
+STRY0011293 pret pour test
